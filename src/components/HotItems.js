72b90bf1140a0415d6e93a0512709cc962b61f84
Deepanshu Saini <57287690+dipanshu-765@users.noreply.github.com>
2020-07-26 12:19:18 +0530

    Update HotItems.js

--- src/components/HotItems.js
+++ src/components/HotItems.js
@@ -22,13 +22,13 @@ class HotItems extends React.Component{
                     HOT-SELLING ITEMS
                 </h4>
                 <div className="hot-item">
-                    <img src={this.state.images[this.props.currentImages[0]]} alt="image-1"></img>
+                    <img src={this.state.images[this.props.currentImages[0]]} alt="hot-1"></img>
                 </div>
                 <div className="hot-item">
-                    <img src={this.state.images[this.props.currentImages[1]]} alt="image-2"></img>
+                    <img src={this.state.images[this.props.currentImages[1]]} alt="hot-2"></img>
                 </div>
                 <div className="hot-item">
-                    <img src={this.state.images[this.props.currentImages[2]]} alt="image-3"></img>
+                    <img src={this.state.images[this.props.currentImages[2]]} alt="hot-3"></img>
                 </div>
             </div>
             )
