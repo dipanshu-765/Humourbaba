05f850c9fd928a49e19cfd191f3f49e25847e440
Deepanshu Saini <57287690+dipanshu-765@users.noreply.github.com>
2020-07-26 12:20:14 +0530

    Update PopularThemes.js

--- src/components/PopularThemes.js
+++ src/components/PopularThemes.js
@@ -8,10 +8,10 @@ class PopularThemes extends React.Component{
                 <h4 id="popular-themes-text">POPULAR THEMES</h4>
                 <br></br>
                 <div id="theme1">
-                    <img src="photo10.jpg" alt="image-10"></img>
+                    <img src="photo10.jpg" alt="Popular Theme 1"></img>
                 </div>
                 <div id="theme2">
-                    <img src="photo11.jpg" alt="image-11"></img>
+                    <img src="photo11.jpg" alt="Popular Theme 2"></img>
                 </div>
             </div>
         )
